8ae5941e7d20f0d6c2bc73363c61eef71ed577ae
flloki97 <fbryma7@gmail.com>
2023-11-29 16:01:27 +0100

    update

--- js/global.js
+++ js/global.js
@@ -1,21 +1,14 @@
 function openMenu() {
-    var mobileMenu = document.getElementById("ham-menu").classList.toggle('opened');
-    var smallMenu = document.getElementById("ham-menu");
-    var styleLineTop = document.getElementById("top-line").style;
-    var styleLineBottom = document.getElementById("bottom-line").style;
-    var dropMenu = document.getElementById("menu-links-container");
+    var mobileMenu = document.getElementById("menu-links-container").classList.toggle('opened');
+    var smallMenu = document.getElementById("menu-links-container");
+    var links = document.getElementById("links-wrapper");
 
     if(smallMenu.classList.contains('opened')) {
-       styleLineTop.webkitTransform ='rotate(45deg) translate(0px, 0px)';
-       styleLineBottom.webkitTransform ='rotate(315deg) translate(7px, -7px)';
-      //  dropMenu.style.display = "flex";
-      // dropMenu.setAttribute("style", "height: 80vh ");
-      dropMenu.classList.toggle("hidden");
+      smallMenu.style.height = "0px";
+      links.style.display = "none";
     } else {
-       styleLineTop.webkitTransform ='rotate(0deg) translate(0px, 0px)';
-       styleLineBottom.webkitTransform ='rotate(0deg) translate(0px, 0px)';
-      //  dropMenu.setAttribute("style", "height: 0vh");
-      //  dropMenu.style.display = "none";
-      dropMenu.classList.toggle("hidden");
+      smallMenu.style.height = "215px";
+      links.style.display = "block";
+      
     } 
   }
