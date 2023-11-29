c6f22fa263010127db3b4ca5cbc4920e4b7d77eb
flloki97 <fbryma7@gmail.com>
2023-11-29 16:09:41 +0100

    update

--- js/global.js
+++ js/global.js
@@ -4,11 +4,11 @@ function openMenu() {
     var links = document.getElementById("links-wrapper");
 
     if(smallMenu.classList.contains('opened')) {
-      smallMenu.style.height = "0px";
-      links.style.display = "none";
-    } else {
       smallMenu.style.height = "215px";
-      links.style.display = "block";
+      document.getElementById("op-cl-menu").src = "../img/close-menu.png";
+    } else {
+      smallMenu.style.height = "0px";
+      document.getElementById("op-cl-menu").src = "../img/ham-menu.png";
       
     } 
   }
